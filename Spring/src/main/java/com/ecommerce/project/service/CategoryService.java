67dbd15cc58d2194ead828abc4ea2f8d4ebfa833
PedroL <pepluisgon01@gmail.com>
2026-03-12 21:02:48 -0300

    CategoryRepository + JPA

--- Spring/src/main/java/com/ecommerce/project/service/CategoryService.java
+++ Spring/src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -7,8 +7,6 @@ import com.ecommerce.project.model.Category;
 public interface CategoryService {
     List<Category> getAllCategories();
     void createCategory(Category category);
-
     String deleteCategory(Long categoryId);
-
     Category updateCategory(Category category, Long categoryId);
 }
